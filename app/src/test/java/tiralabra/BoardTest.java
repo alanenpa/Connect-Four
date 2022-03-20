aa65a9acef4001bca28e0f55d82c16ec57c260be
Patrik Alanen <alanen.patrik@gmail.com>
2022-03-20 20:50:35 +0200

    Fix tests

--- app/src/test/java/tiralabra/BoardTest.java
+++ app/src/test/java/tiralabra/BoardTest.java
@@ -4,82 +4,82 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static tiralabra.App.*;
 
 public class BoardTest {
-    private Board board;
-    private Referee referee;
     private char[][] grid;
 
     @Before
     public void init() {
-        board = new Board();
-        referee = new Referee();
-        grid = board.getGrid();
+        grid = initBoard();
         // horizontal coordinates for columns: 2, 6, 10, 14, 18, 22, 26
     }
 
     @Test
     public void movesShouldGoToCorrectPositions() {
-        board.makeAMove(3, 'O');
+        int col = 2;
+        int row = getNextOpenRow(col, grid);
+        dropPiece(row, col, 'O', grid);
         assertEquals('O', grid[5][2]);
 
-        board.makeAMove(7, 'O');
+        col = 6;
+        row = getNextOpenRow(col, grid);
+        dropPiece(row, col, 'O', grid);
         assertEquals('O', grid[5][6]);
     }
 
     @Test
     public void discsShouldStackUpOnBoard() {
-        char player = referee.useTurn();
-        board.makeAMove(4, player);
-
-        player = referee.useTurn();
-        board.makeAMove(4, player);
-
-        player = referee.useTurn();
-        board.makeAMove(4, player);
-
-        player = referee.useTurn();
-        board.makeAMove(4, player);
-        assertEquals(player, grid[2][3]);
-        board.print();
-
-        player = referee.useTurn();
-        assertEquals(player, grid[3][3]);
+        int col = 4;
+        int row = getNextOpenRow(4, grid);
+        dropPiece(row, col, 'X', grid);
+        row = getNextOpenRow(4, grid);
+        dropPiece(row, col, 'O', grid);
+        row = getNextOpenRow(4, grid);
+        dropPiece(row, col, 'X', grid);
+        row = getNextOpenRow(4, grid);
+        dropPiece(row, col, 'O', grid);
+        assertEquals('O', grid[2][4]);
+        assertEquals('X', grid[3][4]);
     }
 
     @Test
     public void validationShouldAcceptOnlyColumnNumbers() {
-        int result = board.validate("");
+        int result = validateInput("");
         assertEquals(result, -1);
 
-        result = board.validate("x");
+        result = validateInput("x");
         assertEquals(result, -1);
 
-        result = board.validate("17");
+        result = validateInput("17");
         assertEquals(result, -1);
 
-        result = board.validate("0");
+        result = validateInput("0");
         assertEquals(result, -1);
 
-        result = board.validate("8");
+        result = validateInput("8");
         assertEquals(result, -1);
 
-        result = board.validate("3");
-        assertEquals(result, 3);
+        result = validateInput("3");
+        assertEquals(result, 2);
     }
 
     @Test
     public void horizontallyConnectedFourwins() {
-        int[] moves = {4,4,7,6,1,7,2,6,5};
-        char player;
+        int[] moves = {3,3,6,5,0,6,1,5,4};
+        int turn = 0;
+        char piece = 'O';
         for (int i = 0; i < moves.length; i++) {
-            player = referee.useTurn();
-            board.makeAMove(moves[i], player);
-            assertFalse(board.areFourConnected(player));
+            piece = turn == 0 ? 'O' : 'X';
+            int row = getNextOpenRow(moves[i], grid);
+            dropPiece(row, moves[i], piece, grid);
+            turn++;
+            turn = turn % 2;
+            assertFalse(areFourConnected(piece, grid));
         }
-        player = referee.useTurn();
-        board.makeAMove(5, player);
-        assertTrue(board.areFourConnected(player));
+        int row = getNextOpenRow(4, grid);
+        dropPiece(row,4, 'X', grid);
+        assertTrue(areFourConnected('X', grid));
         // Final situation
         // |   |   |   |   |   |   |   |
         // |   |   |   |   |   |   |   |
@@ -91,16 +91,21 @@ public class BoardTest {
 
     @Test
     public void verticallyConnectedFourwins() {
-        int[] moves = {3,4,3,6,3,7};
-        char player;
+        int[] moves = {2,3,2,5,2,6};
+        int turn = 0;
+        char piece = 'O';
         for (int i = 0; i < moves.length; i++) {
-            player = referee.useTurn();
-            board.makeAMove(moves[i], player);
-            assertFalse(board.areFourConnected(player));
+            piece = turn == 0 ? 'O' : 'X';
+            int row = getNextOpenRow(moves[i], grid);
+            dropPiece(row, moves[i], piece, grid);
+            turn++;
+            turn = turn % 2;
+            assertFalse(areFourConnected(piece, grid));
         }
-        player = referee.useTurn();
-        board.makeAMove(3, player);
-        assertTrue(board.areFourConnected(player));
+        int row = getNextOpenRow(2, grid);
+        dropPiece(row, 2, 'O', grid);
+        print(grid);
+        assertTrue(areFourConnected('O', grid));
         // Final situation
         // |   |   |   |   |   |   |   |
         // |   |   |   |   |   |   |   |
@@ -112,16 +117,20 @@ public class BoardTest {
 
     @Test
     public void DiagonallyAscendingConnectedFourwins() {
-        int[] moves = {3,3,4,1,4,4,5,5,5,5,7,6,6,6,1,6,7};
-        char player;
+        int[] moves = {2,2,3,0,3,3,4,4,4,4,6,5,5,5,0,5,6};
+        int turn = 0;
+        char piece = 'O';
         for (int i = 0; i < moves.length; i++) {
-            player = referee.useTurn();
-            board.makeAMove(moves[i], player);
-            assertFalse(board.areFourConnected(player));
+            piece = turn == 0 ? 'O' : 'X';
+            int row = getNextOpenRow(moves[i], grid);
+            dropPiece(row, moves[i], piece, grid);
+            turn++;
+            turn = turn % 2;
+            assertFalse(areFourConnected(piece, grid));
         }
-        player = referee.useTurn();
-        board.makeAMove(6, player);
-        assertTrue(board.areFourConnected(player));
+        int row = getNextOpenRow(5, grid);
+        dropPiece(row, 5, 'X', grid);
+        assertTrue(areFourConnected('X', grid));
         // Final situation
         // |   |   |   |   |   |   |   |
         // |   |   |   |   |   | X |   |
@@ -133,17 +142,20 @@ public class BoardTest {
 
     @Test
     public void DiagonallyDescendingConnectedFourwins() {
-        int[] moves = {1,1,1,1,5,4,3,3,6,2,2};
-        char player;
+        int[] moves = {0,0,0,0,4,3,2,2,5,1,1};
+        int turn = 0;
+        char piece = 'O';
         for (int i = 0; i < moves.length; i++) {
-            player = referee.useTurn();
-            board.makeAMove(moves[i], player);
-            assertFalse(board.areFourConnected(player));
+            piece = turn == 0 ? 'O' : 'X';
+            int row = getNextOpenRow(moves[i], grid);
+            dropPiece(row, moves[i], piece, grid);
+            turn++;
+            turn = turn % 2;
+            assertFalse(areFourConnected(piece, grid));
         }
-        player = referee.useTurn();
-        board.makeAMove(2, player);
-        board.print();
-        assertTrue(board.areFourConnected(player));
+        int row = getNextOpenRow(1, grid);
+        dropPiece(row, 1, 'X', grid);
+        assertTrue(areFourConnected('X', grid));
         // Final situation
         // |   |   |   |   |   |   |   |
         // |   |   |   |   |   |   |   |
