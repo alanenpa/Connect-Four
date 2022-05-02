96aa24a38e685cf6c9f54f12fc7fe46f0b6b268a
Patrik Alanen <alanen.patrik@gmail.com>
2022-05-02 23:09:34 +0200

    Refactor and divide main game loop

--- connect_four/src/main/java/domain/Game.java
+++ connect_four/src/main/java/domain/Game.java
@@ -1,6 +1,5 @@
 package domain;
 
-import java.util.Random;
 import java.util.Scanner;
 
 import static domain.Board.*;
@@ -10,54 +9,75 @@ import static utils.Utils.validateInput;
 
 public class Game {
 
-    static final int PLAYER = 0;
-    static final int AI = 1;
-
     static final char PLAYER_PIECE = 'X';
     static final char AI_PIECE = 'O';
 
     public static void play() {
         char[][] board = initBoard();
-        Random rand = new Random();
-        int turn = rand.nextInt(AI + 1);
         Scanner scanner = new Scanner(System.in);
-        print(board);
-        char piece = '.';
+        char piece = Math.random() < 0.5 ? 'X' : 'O';
+        int row;
+        int column;
         boolean gameOver = false;
+        System.out.println("quit the game with q");
+        print(board);
 
         while (!gameOver) {
-            if (turn == PLAYER) {
-                System.out.println("It's X's turn!");
-                piece = PLAYER_PIECE;
-                System.out.println("Choose a column to play (1-7)");
-                String input = scanner.nextLine();
-                int inputCol = validateInput(input);
-                if (inputCol == -1) break;
-                if (isValidLocation(inputCol, board)) {
-                    int row = getNextOpenRow(inputCol, board);
-                    dropPiece(row, inputCol, piece, board);
-                    if (isAWinningMove(row, inputCol, piece, board)) gameOver = true;
-                }
-                turn++;
-                turn = turn % 2;
-            } else {
-                System.out.println("It's O's turn!");
-                piece = AI_PIECE;
-                int[] arr = minimax(5, piece, Integer.MIN_VALUE, Integer.MAX_VALUE,  board);
-                int column = arr[0];
-                int row = getNextOpenRow(column, board);
-                dropPiece(row, column, piece, board);
-                if (isAWinningMove(row, column, piece, board)) gameOver = true;
-                turn++;
-                turn = turn % 2;
+            announceTurn(piece);
+            column = pickColumn(board, scanner, piece);
+            if (column == -1) {
+                System.out.println("Invalid input!");
+                print(board);
+                continue;
+            } else if (column == -2) {
+                // quitting game with 'q'
+                gameOver = true;
+                continue;
             }
-            print(board);
-        }
-        if (getValidLocations(board).size() == 0) {
-            System.out.println("It's a draw");
-        } else {
-            System.out.println(piece + " wins!");
+            if (isValidLocation(column, board)) {
+                row = useTurn(board, piece, column);
+                if (isAWinningMove(row, column, piece, board)) {
+                    System.out.println(piece + " wins!");
+                    gameOver = true;
+                }
+            } else {
+                continue;
+            }
+            piece = piece == PLAYER_PIECE ? AI_PIECE : PLAYER_PIECE;
+            if (getValidLocations(board).size() == 0) {
+                System.out.println("It's a draw");
+            }
         }
         System.out.println("Thanks for playing!");
+    }
+
+    private static int useTurn(char[][] board, char piece, int column) {
+        int row;
+        row = getNextOpenRow(column, board);
+        dropPiece(row, column, piece, board);
+        print(board);
+        return row;
+    }
+
+    private static int pickColumn(char[][] board, Scanner scanner, char piece) {
+        int column;
+        if (piece == PLAYER_PIECE) {
+            String input = scanner.nextLine();
+            column = validateInput(input);
+        } else {
+            int[] arr = minimax(8, piece, Integer.MIN_VALUE, Integer.MAX_VALUE, board);
+            column = arr[0];
+        }
+        return column;
+    }
+
+    public static void announceTurn(char piece) {
+        if (piece == PLAYER_PIECE) {
+            System.out.println("It's X's turn!");
+            System.out.println("Choose a column to play (1-7)");
+            System.out.println("or quit the game with q");
+        } else {
+            System.out.println("It's O's turn!");
+        }
     }
 }
